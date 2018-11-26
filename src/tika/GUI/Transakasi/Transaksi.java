0ab3ba99e86becb6d6088624b2f38926088efc5a
TikaYulianti <tikayulian27@gmail.com>
2018-11-26 23:04:27 +0700

    membuat class Transaksi

--- src/tika/GUI/Transakasi/Transaksi.java
+++ src/tika/GUI/Transakasi/Transaksi.java
@@ -10,3 +10,35 @@ import java.util.*;
  * @author Tika Yln
  */
 public class Transaksi {
+    
+    private final String code;
+    private ArrayList<Item> items = new ArrayList<>();
+    private float total;
+
+    public Transaksi(String code, float total) {
+        this.code = code;
+        this.total = total;
+    }
+    
+    public void setTotal(){
+        float total = 0;
+        for(Item item : this.items){
+            total += item.getTotal();
+        }
+        this.total = total;
+    }
+    
+    public String Info(){
+        setTotal();
+        String str = "";
+        str += "Kode\t\t:"+ this.code +"\n";
+        str += "Daftar Belanja :\n";
+        for (Item item : this.items){
+            str += "\t"+ item.getNama() + "(x" + item.getJumlah()+"):"+ item.getTotal()+"\n";
+        }
+        str += "Total\t\t:" + this.total;
+        return str;
+    }
+    
+    
+}
