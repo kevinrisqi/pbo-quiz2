e244dbdafed8bb66ea107da7518d9aead9be97d4
FauzanAr <faurinta12345@gmail.com>
2018-11-27 07:20:56 -0500

    Pembuatan class Input Data

--- src/com/fauzan/quiz2/pbo/InputData.java
+++ src/com/fauzan/quiz2/pbo/InputData.java
@@ -9,15 +9,38 @@ package com.fauzan.quiz2.pbo;
  *
  * @author fauzan
  */
+import com.sun.glass.ui.Size;
+import java.util.*;
 public class InputData {
-    Object [][] DataPelanggan;
+    ArrayList<Data> dataPelanggan;
     
     public InputData(){
-        
+        dataPelanggan = new ArrayList();
+    }
+    
+    public int duplicateChecking(String nama, int jumlah){
+            for (int i = 0;i<dataPelanggan.size();i++){
+                if (dataPelanggan.get(i).namaBarang.contains(nama)){
+                    return i;
+            }
+        }
+            return -1;
     }
     
     public void isiData(String nama, int jumlah, int harga){
-        Data data = new Data(nama,jumlah,harga);
+        int tmp = duplicateChecking (nama,jumlah);
         
+        if (tmp >= 0){
+            jumlah += dataPelanggan.get(tmp).jumlahBarang;
+            Data data = new Data(nama,jumlah,harga);
+            dataPelanggan.add(data);
+        }else{
+            Data data = new Data(nama,jumlah,harga);
+            dataPelanggan.add(data);
+        }
+    }
+    
+    public ArrayList<Data> getData(){
+        return dataPelanggan;
     }
 }
