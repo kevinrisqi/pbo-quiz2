353ca8c7b82da1f947bd128ea1340858129a1804
FauzanAr <faurinta12345@gmail.com>
2018-11-26 23:18:54 -0500

    Pembuatan class data

--- src/com/fauzan/quiz2/pbo/Data.java
+++ src/com/fauzan/quiz2/pbo/Data.java
@@ -9,6 +9,17 @@ package com.fauzan.quiz2.pbo;
  *
  * @author fauzan
  */
+
+import java.util.*;
 public class Data {
+    ArrayList<String> namaBarang = new ArrayList<String>();
+    ArrayList<Integer> jumlahBarang = new ArrayList<Integer>();
+    ArrayList<Integer> hargaBarang = new ArrayList<Integer>();
+    
+    public Data(String namaBarang, int jumlahBarang, int hargaBarang){
+        this.namaBarang.add(namaBarang);
+        this.jumlahBarang.add(jumlahBarang);
+        this.hargaBarang.add(hargaBarang);
+    }
     
 }
