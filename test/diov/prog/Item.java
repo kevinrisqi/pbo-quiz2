2548eda98554ed9166ad529d4c1e1654333dd944
user.com <user.com@Dhyo>
2018-11-30 16:18:20 +0700

    Class Item

---
+++ test/diov/prog/Item.java
@@ -0,0 +1,62 @@
+package diov.prog;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author falcao
+ */
+public class Item {
+    
+    private String nama;
+    private int jumlah;
+    private double harga;
+
+    public Item(String nama, int jumlah, double harga) {
+        this.nama = nama;
+        this.jumlah = jumlah;
+        this.harga = harga;
+    }
+    
+    public Item(){
+        
+    }
+
+
+     @Override
+        public String toString() {
+            return nama;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    public double getTotal(){
+        return this.harga*this.jumlah;
+    }
+}
