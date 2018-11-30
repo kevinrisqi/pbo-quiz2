48d3c22e2d6b8e6c8989b6f058c6af7b56802654
dhyojr <dhyojr11@gmail.com>
2018-11-30 23:38:04 +0700

    class combobox

--- test/diov/prog/Penjualan.java
+++ test/diov/prog/Penjualan.java
@@ -1,69 +1,53 @@
+package diov.prog;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package diov.prog;
-
-import java.util.*;
-import javax.swing.table.DefaultTableModel;
 
 /**
  *
  * @author falcao
  */
-public final class Penjualan { 
-  
-    private String code;
-    private final ArrayList<Item> items= new ArrayList<>();
-    private double total;
-    private DefaultTableModel tabel = new DefaultTableModel();
+import java.util.ArrayList;
+public class Penjualan {
+    //variable untuk menampilkan kode pembayaran 
+    private final String kode;
+    //item untuk menampilkan item apa saja yang dibeli
+    private  ArrayList<Item> items = new ArrayList();
+    //variable menampilkan total pembelian
+    private float total;
     
-
-    public Penjualan(String code, double total) {
-        this.code = code;
-        this.total = total;
-    }
+    //konstruktor
 
     public Penjualan() {
-        getTabel().addColumn("Nama");
-        getTabel().addColumn("Harga");
-        getTabel().addColumn("Jumlah");
-    } 
-    
-    
-
-    public String getCode() {
-        return code;
-    }
-
-    public DefaultTableModel getTabel() {
-        return tabel;
-    }
-
-    public void setTabel(DefaultTableModel tabel) {
-        this.tabel = tabel;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
+        this.kode = null;
     }
     
-    public String Daftar() { 
-        getTotal();
-        String moza = "";
-        moza += "Code\t\t: "+ this.code +"\n";
-        moza += "List Belanja : \n";
-        moza = this.items.stream().map((item) -> "\t"+ item.getNama() +"(z"+ item.getJumlah() +") : "+ item.getTotal() +"\n").reduce(moza, String::concat);
-        moza += "Total\t\t: "+ this.total;
-        return moza;
+
+    public Penjualan(String kode, ArrayList<Item> items) {
+        this.kode = kode;
+        this.items = items;
     }
+    
+    //total setter
+    public void setTotal(){
+        double total = 0;
+        for(Item item : this.items){
+            total += item.getTotal();
+        }
+        this.total = (float) total;
+    }
+    //output transaksi
+    public String Pembayaran(){
+        setTotal();
+        String x = "";
+        x += "Kode\t\t : " + this.kode + "\n";
+        x += "Daftar Belanja : \n ";
+        x = this.items.stream().map((item) -> "\t" + item.getNama()  + "(" + item.getJumlah() + ") : " +item.getTotal() + "\n").reduce(x, String::concat);
+        x += "Total\t\t : " + this.total;
+        return x;
+    }
+    
 }
