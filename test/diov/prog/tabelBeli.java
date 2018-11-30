6ae6d6b70b641974277850f7530353f8d8e942fc
dhyojr <dhyojr11@gmail.com>
2018-11-30 23:36:46 +0700

    class tabel

---
+++ test/diov/prog/tabelBeli.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package diov.prog;
+
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author user.com
+ */
+public class tabelBeli extends DefaultTableModel{
+    //berfungsi untuk menyimpan nama kolom
+    private String[] kolom;
+    
+    //konstruktor
+
+    public tabelBeli() {
+        this.kolom = new String[]{
+          "Nama" , "Harga"  , "Jumlah"
+        };
+    }
+    //kolomNama getter
+    public String[] getKolomNama(){
+        return this.kolom;
+    }
+}
